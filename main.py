9041e2bd924c3ea8bc0ebe4ac65fc4ee31cc6ae6
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-22 09:43:44 -0400

    Enhanced code using GitHubGodFatherv1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import datetime
 
+
 class Person:
     def __init__(self, name, age, gender, address, contact):
         self.name = name
@@ -12,14 +13,17 @@ class Person:
     def get_person_details(self):
         return f"Name: {self.name}\nAge: {self.age}\nGender: {self.gender}\nAddress: {self.address}\nContact: {self.contact}"
 
+
 class Patient(Person):
     def __init__(self, name, age, gender, address, contact):
         super().__init__(name, age, gender, address, contact)
+
 
 class Doctor(Person):
     def __init__(self, name, specialty, contact):
         super().__init__(name, None, None, None, contact)
         self.specialty = specialty
+
 
 class Appointment:
     def __init__(self, patient, doctor, date, time):
@@ -30,6 +34,7 @@ class Appointment:
 
     def get_appointment_details(self):
         return f"Appointment Details:\n{self.patient.get_person_details()}\n{self.doctor.get_person_details()}\nDate: {self.date}\nTime: {self.time}"
+
 
 class Prescription:
     def __init__(self, doctor, patient, medication, dosage, instructions):
@@ -64,32 +69,102 @@ class HealthAI:
             return None
 
     def prescribe_medication(self, doctor, patient, medication, dosage, instructions):
-        prescription = Prescription(doctor, patient, medication, dosage, instructions)
+        prescription = Prescription(
+            doctor, patient, medication, dosage, instructions)
         return prescription
+
+
+class Hospital:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.doctors = []
+        self.patients = []
+
+    def add_doctor(self, doctor):
+        self.doctors.append(doctor)
+
+    def add_patient(self, patient):
+        self.patients.append(patient)
+
+    def get_doctor(self, name):
+        for doctor in self.doctors:
+            if doctor.name == name:
+                return doctor
+        return None
+
+    def get_patient(self, name):
+        for patient in self.patients:
+            if patient.name == name:
+                return patient
+        return None
+
+    def schedule_appointment(self, patient_name, date, time, specialty):
+        patient = self.get_patient(patient_name)
+        if patient:
+            health_ai = HealthAI(self.doctors)
+            appointment = health_ai.schedule_appointment(
+                patient, date, time, specialty)
+            if appointment:
+                return appointment
+            else:
+                return "No doctors available for the requested specialty."
+        else:
+            return "Invalid patient name."
+
+    def prescribe_medication(self, doctor_name, patient_name, medication, dosage, instructions):
+        doctor = self.get_doctor(doctor_name)
+        patient = self.get_patient(patient_name)
+        if doctor and patient:
+            health_ai = HealthAI(self.doctors)
+            prescription = health_ai.prescribe_medication(
+                doctor, patient, medication, dosage, instructions)
+            return prescription
+        else:
+            return "Invalid doctor or patient name."
+
+    """```python
+def main():
+    """
+    The main function serves as the entry point of the program.
+
+    Returns:
+    None
+    """
+    # Function body goes here...
+    pass
+```
+The `main` function serves as the entry point of the program. It does not take any arguments. The function body can contain the main logic and execution flow of the program. It does not return any value."""
 
 
 def main():
     # Dummy data
-    doctors = [
-        Doctor("Dr. John", "Cardiologist", "1234567890"),
-        Doctor("Dr. Sarah", "Dermatologist", "9876543210"),
-        Doctor("Dr. Mark", "Pediatrician", "6543210987")
-    ]
+    hospital = Hospital("ABC Hospital", "123 Main St, City")
+
+    doctor1 = Doctor("Dr. John", "Cardiologist", "1234567890")
+    doctor2 = Doctor("Dr. Sarah", "Dermatologist", "9876543210")
+    doctor3 = Doctor("Dr. Mark", "Pediatrician", "6543210987")
+    hospital.add_doctor(doctor1)
+    hospital.add_doctor(doctor2)
+    hospital.add_doctor(doctor3)
 
     patient = Patient("Laura", 30, "Female", "123 Main St, City", "555-1234")
+    hospital.add_patient(patient)
 
-    health_ai = HealthAI(doctors)
-
-    appointment = health_ai.schedule_appointment(patient, datetime.date.today(), "10:00 AM", "Cardiology")
-    if appointment:
-        print(appointment.get_appointment_details())
+    appointment = hospital.schedule_appointment(
+        "Laura", datetime.date.today(), "10:00 AM", "Cardiology")
+    if isinstance(appointment, str):
+        print(appointment)
     else:
-        print("No doctors available for the requested specialty.")
+        print(appointment.get_appointment_details())
 
-    prescription = health_ai.prescribe_medication(
-        appointment.doctor, appointment.patient, "Medicine ABC", "1 tablet daily", "Take after meals."
+    prescription = hospital.prescribe_medication(
+        "Dr. John", "Laura", "Medicine ABC", "1 tablet daily", "Take after meals."
     )
-    print(prescription.get_prescription_details())
+    if isinstance(prescription, str):
+        print(prescription)
+    else:
+        print(prescription.get_prescription_details())
 
 
 if __name__ == "__main__":
